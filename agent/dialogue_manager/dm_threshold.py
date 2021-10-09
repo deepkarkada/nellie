39d1295df5c0085c55d99347735dc0b9bea35271
MaikePaetzel <paetzel-pruesmann@uni-potsdam.de>
2021-10-09 21:25:28 +0200

    DM is now producing "TargetIdentified" DA only when Threshold is crossed

--- agent/dialogue_manager/dm_threshold.py
+++ agent/dialogue_manager/dm_threshold.py
@@ -3,6 +3,8 @@
 from retico.core import abstract
 from retico.core.dialogue.common import DialogueActIU
 
+THRESHOLD_IDENTIFIED = 0.3
+THRESHOLD_SKIP = 5
 
 class DialogueManager(abstract.AbstractModule):
     """Currently a dummy DM Module
@@ -25,9 +27,20 @@ class DialogueManager(abstract.AbstractModule):
     def output_iu():
         return DialogueActIU
 
+    def read_country_ids(self):
+        self.country_to_ids = {}
+        self.ids_to_country = {}
+        with open("../data/agent/countrynames_id.txt") as f:
+            for line in f:
+                (key, val) = line.split("\t")
+                val = val.replace("\n","")
+                self.country_to_ids[val] = key
+                self.ids_to_country[key] = val
+
     def __init__(self, incremental=True, **kwargs):
         super().__init__(**kwargs)
         self.incremental = incremental
+        self.read_country_ids()
 
     def get_current_dialogue_act(self, input_iu):
         return input_iu.act, input_iu.concepts, input_iu.confidence
@@ -36,8 +49,16 @@ class DialogueManager(abstract.AbstractModule):
         act, concepts, confidence = self.get_current_dialogue_act(input_iu)
         if not act:
             return None
-        print("DM is processing a DA of type " + str(act) + " with the following values - Concept: " + str(concepts) + "; Confidence: " + str(confidence))
 
-        output_iu = self.create_iu(input_iu)
-        output_iu.set_act("TargetIdentified", "UK")
-        return output_iu
+        if act == "TargetDescription":
+            """This is input from the Description NLU"""
+            pred_entities = concepts["pred_entities"]
+            country_max_conf = max(pred_entities, key=pred_entities.get)
+            max_conf = pred_entities[country_max_conf]
+
+            print("DM is processing a DA of type " + str(act) + " with max confidence country for " + str(country_max_conf) + " (" + str(max_conf) + ")")
+
+            if max_conf > THRESHOLD_IDENTIFIED:
+                output_iu = self.create_iu(input_iu)
+                output_iu.set_act("TargetIdentified", self.country_to_ids[country_max_conf])
+                return output_iu
